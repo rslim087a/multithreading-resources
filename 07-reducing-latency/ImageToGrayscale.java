40cbd3a538fc0ec5b715125521d022ab038bfde8
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2023-11-20 01:43:35 -0500

    reducing latency

---
+++ 07-reducing-latency/ImageToGrayscale.java
@@ -0,0 +1,43 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+
+public class ImageToGrayscale {
+
+    public static void main(String[] args) throws Exception {
+        File inputFile = new File("beach.png"); // Input file
+        File outputFile = new File("beach_grayscale.png"); // Output file
+
+        try {
+            BufferedImage image = ImageIO.read(inputFile);
+            convertToGrayscale(image);
+
+            ImageIO.write(image, "png", outputFile);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    private static void convertToGrayscale(BufferedImage image) {
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+                Color color = new Color(rgb, true);
+    
+                int red = color.getRed();
+                int green = color.getGreen();
+                int blue = color.getBlue();
+                
+                // Compute the average of red, green, and blue (simple grayscale)
+                int gray = (red + green + blue) / 3;
+                
+                // Create new grayscale color
+                Color grayColor = new Color(gray, gray, gray, color.getAlpha());
+                image.setRGB(x, y, grayColor.getRGB());
+            }
+        }
+    }
+}
