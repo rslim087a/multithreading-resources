7f39be16128362c8a99b95cdf30c103ab99a3498
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2023-11-20 02:04:36 -0500

    increasing-throughput

---
+++ 08-increasing-throughput/FactorialHttpServer.java
@@ -0,0 +1,30 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+
+public class FactorialHttpServer {
+
+    public static void main(String[] args) throws Exception {
+        // Create an HTTP server listening on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        // Create a context that listens for requests on /factorial
+        server.createContext("/factorial", (HttpExchange exchange) -> {
+                try {
+                    Thread.sleep(5); // Simulating a process that takes time
+                    exchange.sendResponseHeaders(200, "HELLO WORLD".getBytes().length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write("HELLO WORLD".getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+}
