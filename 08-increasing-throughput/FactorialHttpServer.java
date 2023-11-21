e36c3b918813c659826ce11449557de766060cb8
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2023-11-21 11:23:35 -0500

    increasing-throughput

--- 08-increasing-throughput/FactorialHttpServer.java
+++ 08-increasing-throughput/FactorialHttpServer.java
@@ -1,29 +1,40 @@
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FactorialHttpServer {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         // Create an HTTP server listening on port 8080
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         // Create a context that listens for requests on /factorial
         server.createContext("/factorial", (HttpExchange exchange) -> {
-                try {
-                    Thread.sleep(5); // Simulating a process that takes time
-                    exchange.sendResponseHeaders(200, "HELLO WORLD".getBytes().length);
-                    OutputStream os = exchange.getResponseBody();
-                    os.write("HELLO WORLD".getBytes());
-                    os.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+            try {
+                Thread.sleep(5); // Simulating a process that takes time
+                exchange.sendResponseHeaders(200, "HELLO WORLD".getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write("HELLO WORLD".getBytes());
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
+        // Create a thread pool with a number of threads equal to the number of available processors
+        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        // Set the server's executor to this thread pool
+        server.setExecutor(threadPool);
+
+        // Start the server
         server.start();
         System.out.println("Server started on port 8080");
     }
 }
+
+
